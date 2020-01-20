56bc4e8bb4a68b5adcce95920af04dfe7b74b6a1
Gary Alberto Cordero Rosa <garycordero1690@gmail.com>
2020-01-20 06:25:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -182,15 +182,7 @@ end
 
 def most_points_scored
     players = get_all_players(game_hash)
-    player_with_most_points_score = {}
-    current_biggest_score = 0
-    players.each do |player|
-      if(player[:points] > current_biggest_score)
-        current_biggest_score = player[:points]
-        player_with_most_points_score = player
-      end
-    end
-    player_with_most_points_score[:player_name]
+    player_with_the_most(players){|x| x[:points]}[:name]
 end
 
 def winning_team
